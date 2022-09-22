d2a71ba6a6a523fc176ff31ac96882faf22c3af0
Julien <mianojulien57@gmail.com>
2022-09-22 14:51:24 +0200

    modif en quizz openclassroom

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// Modify the code here
+// ======================
+const total = 0;
 
+const calculateAverageRating = (number) => {};
+
+calculateAverageRating(tauRatings);
+
+// ======================
+
+const tauRatings = [5, 4, 5, 5, 1, 2];
+const colinRatings = [5, 5, 5, 4, 5];
+
+const tauAverage = calculateAverageRating(tauRatings);
+const colinAverage = calculateAverageRating(colinRatings);
+
+if (tauAverage && colinAverage) {
+  document.querySelector("#tau-score").innerText =
+    tauAverage.toFixed(1) + " stars";
+  document.querySelector("#colin-score").innerText =
+    colinAverage.toFixed(1) + " stars";
+  document.querySelector("#clara-score").innerText = `${
+    calculateAverageRating([]) === 0
+      ? "No ratings"
+      : calculateAverageRating([]) + " stars"
+  }`;
+}
